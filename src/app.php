f11137f76706b100d02c4e40dd22f79ecc013f1a
frian <a@frian.org>
2014-02-06 15:44:47 +0100

    modified app.php

--- src/app.php
+++ src/app.php
@@ -3,19 +3,73 @@
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\SwiftmailerServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+
 
 $app = new Application();
-$app->register(new UrlGeneratorServiceProvider());
-$app->register(new ValidatorServiceProvider());
-$app->register(new ServiceControllerServiceProvider());
+
+/*
+ *  -- register twig templating -----------------------------------------------
+*/
 $app->register(new TwigServiceProvider());
 
+/*
+ *  -- register url generator -------------------------------------------------
+*/
+$app->register(new UrlGeneratorServiceProvider());
+
+/*
+ *  -- register form generator ------------------------------------------------
+*/
+$app->register(new FormServiceProvider());
+
+/*
+ *  -- register swiftmailer ---------------------------------------------------
+*/
+$app->register(new SwiftmailerServiceProvider());
+
+// configure smtp
+$app['swiftmailer.options'] =
+array(
+		'host'       => 'your-mail-server',
+		'port'       => 'your-mail-port',
+		'username'   => 'your-username',
+		'password'   => 'your-password',
+		'encryption' => null,
+		'auth_mode'  => null
+);
+
+/*
+ *  -- register validator -----------------------------------------------------
+*/
+$app->register(new ValidatorServiceProvider());
+
+/*
+ *  -- register translator ----------------------------------------------------
+*/
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+		'locale' => 'fr',
+));
+
+/*
+ *  -- EXAMPLE : load french validator message --------------------------------
+*/
+$app->before(function () use ($app) {
+	$app['translator']->addLoader('xlf', new Symfony\Component\Translation\Loader\XliffFileLoader());
+  $app['translator']->addResource(
+	  'xlf',
+	  __DIR__ . '/../vendor/symfony/validator/Symfony/Component/Validator/Resources/translations/validators.fr.xlf',
+	  'fr',
+	  'validators');
+});
 
 
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+
     // add custom globals, filters, tags, ...
 
     return $twig;
